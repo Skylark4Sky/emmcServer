1f03c561a9fe48940771d0360b8136eb904934b7
johan <johaness@qq.com>
2020-12-17 22:17:47 +0800

    fix some brk

--- handle/device/device_charge_handle.go
+++ handle/device/device_charge_handle.go
@@ -61,20 +61,16 @@ func comChargeTaskStop(iface interface{}, deviceSN string, deviceID uint64, ack 
 		if !ack {
 			entity := iface.(*mqtt.ComTaskStopTransfer)
 			Redis().GetDeviceComDataFormRedis(deviceSN, entity.ComID, cacheData)
-
-			SystemLog("comChargeTaskStop: ", cacheData)
-
 			deviceCom.Create(deviceID, uint64(entity.Token), entity.ComID)
+			deviceCom.Init(cacheData.MaxEnergy, cacheData.MaxTime, uint32(cacheData.MaxElectricity))
 			deviceCom.ChangeValue(cacheData.UseEnergy, cacheData.UseTime, cacheData.MaxChargeElectricity, cacheData.AveragePower, cacheData.MaxPower)
 			task.RunTaskWithTypeAndEntity(ASYNC_STOP_COM_CHARGE_TASK, deviceCom)
 		} else {
 			comList := iface.(*mqtt.ComList)
 			entity := (comList.ComPort[0]).(mqtt.ComData)
 			Redis().GetDeviceComDataFormRedis(deviceSN, entity.Id, cacheData)
-
-			SystemLog("comChargeTaskStop ack: ", cacheData)
-
 			deviceCom.Create(deviceID, uint64(entity.Token), entity.Id)
+			deviceCom.Init(cacheData.MaxEnergy, cacheData.MaxTime, uint32(cacheData.MaxElectricity))
 			deviceCom.ChangeValue(cacheData.UseEnergy, cacheData.UseTime, cacheData.MaxChargeElectricity, cacheData.AveragePower, cacheData.MaxPower)
 			task.RunTaskWithTypeAndEntity(ASYNC_STOP_COM_CHARGE_TASK_ACK, deviceCom)
 		}
